84d2e792d60ef1e8aa1f0de25d8eda3645ef315b
nicoletheillustrator <nicole-park@naver.com>
2022-12-09 23:05:31 +0900

    www

--- src/components/header.js
+++ src/components/header.js
@@ -2,11 +2,11 @@ import React from "react";
 import headjson from "../data/headlines/mainhead.json"
 function Header() {
 
-    bgImage = {
-        backgroundImage: `url("${headjson.image}")`
-    }
+    // bgImage = {
+    //     backgroundImage: `url("${headjson.image}")`
+    // }
     return(
-        <div className="header" style={bgImage}>
+        <div className="header">
             <div className="header-wrap">
                 <h1>Sincerus</h1>
                 <h2>{headjson.description}</h2>
